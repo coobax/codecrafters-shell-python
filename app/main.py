3b241d283093d2acd2f6711fd612b7e7a73c5c29
Dominik Hasselbusch <dominikhasselbusch@gmail.com>
2026-01-23 21:41:40 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import sys
 
+BUILTINS = {
+    "exit": lambda code=0, *_: sys.exit(int(code)),
+    "echo": lambda *args: print(" ".join(args)),
+}
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        pass
+
     # Wait for user input
-        user_Input = input()
-        if user_Input.strip() == "exit":
-            break
-        print(f"{user_Input}: command not found")
+        user_Input = input().split()
+
+        cmd = user_Input[0]
+
+        args = user_Input[1:]
+        
+        if cmd in BUILTINS:
+            BUILTINS[cmd](*args)
+        else:    
+            print(f"{cmd}: command not found")
 
 if __name__ == "__main__":
     main()
