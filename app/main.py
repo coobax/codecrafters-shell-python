f9edfb20856bbb42f6c793a008b8b6c3421997d2
Dominik Hasselbusch <dominikhasselbusch@gmail.com>
2026-01-23 19:15:17 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
     pass
-
+ # Wait for user input
+    user_Input = input()
+    print(f"{user_Input}: command not found")
 
 if __name__ == "__main__":
     main()
