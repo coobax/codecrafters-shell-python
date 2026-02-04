7b1bf28c481e951b0a72839b1aff9e94ad273efa
Dominik Hasselbusch <dominikhasselbusch@gmail.com>
2026-02-04 22:19:34 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,28 +2,44 @@ import sys
 import os
 import subprocess
 
-def find_executable(cmd):
+def find_executable(exec_name):
     paths = os.environ.get("PATH", "").split(os.pathsep)
     for path in paths:
-        full_path = os.path.join(path, cmd)
+        full_path = os.path.join(path, exec_name)
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
             return full_path
-    return None
+    return
 
-def sh_type(cmd):
-    executable_path = find_executable(cmd)
-    if cmd in BUILTINS:
-        print(f"{cmd} is a shell builtin")
+def sh_type(cmd_name):
+    executable_path = find_executable(cmd_name)
+    if cmd_name in BUILTINS:
+        print(f"{cmd_name} is a shell builtin")
     elif executable_path is not None:
-        print(f"{cmd} is {executable_path}")
+        print(f"{cmd_name} is {executable_path}")
     else:
-        print(f"{cmd}: not found")
+        print(f"{cmd_name}: not found")
+
+def sh_cd(*cd_path):
+    if len(cd_path) == 0:
+        cd_path = os.path.expanduser("~")
+        os.chdir(cd_path)
+    elif len(cd_path) == 1:
+        try:
+            cd_path = os.path.expanduser(cd_path[0])
+            os.chdir(cd_path)
+        except OSError:
+            print(f"cd: {cd_path}: No such file or directory")
+            return
+    elif len(cd_path) > 1:
+        print("cd: too many arguments")
+        return
 
 BUILTINS = {
     "exit": lambda code=0, *_: sys.exit(int(code)),
     "echo": lambda *args: print(" ".join(args)),
     "type": sh_type,
     "pwd": lambda *args: print(os.getcwd()),
+    "cd": sh_cd,
 }
 
 def main():
