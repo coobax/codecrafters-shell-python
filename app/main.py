df505ce889002cce52b0dffc6ec33456243d4ed9
Dominik Hasselbusch <dominikhasselbusch@gmail.com>
2026-02-07 17:33:25 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def find_executable(exec_name):
         full_path = os.path.join(path, exec_name)
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
             return full_path
-    return
+    return None
 
 def sh_type(cmd_name):
     executable_path = find_executable(cmd_name)
@@ -19,36 +19,45 @@ def sh_type(cmd_name):
     else:
         print(f"{cmd_name}: not found")
 
-def sh_cd(*cd_path):
-    if len(cd_path) == 0:
-        cd_path = os.path.expanduser("~")
-        os.chdir(cd_path)
-    elif len(cd_path) == 1:
-        try:
-            cd_path = os.path.expanduser(cd_path[0])
-            os.chdir(cd_path)
-        except OSError:
-            print(f"cd: {cd_path}: No such file or directory")
-            return
-    elif len(cd_path) > 1:
+def sh_cd(*args):
+    if len(args) == 0:
+        target = os.path.expanduser("~")
+    elif len(args) == 1:
+        target = os.path.expanduser(args[0])
+    else:
         print("cd: too many arguments")
         return
+    try:
+        os.chdir(target)
+    except OSError:
+        print(f"cd: {target}: No such file or directory")
 
-#No shlex for learning purposes, so we implement our own simple parser that handles single quotes and whitespace
+#No shlex for learning and fun, so we implement our own simple parser that handles single quotes and whitespace
 def parse_line(line):
+    esc_char = False
     in_single = False
     in_double = False
     tkn_active = False
     cur_tkn = []
     tokens = []
-    for i in range(len(line)):
-        if line[i] == "'" and not in_double:
+    for ch in line:
+        if esc_char:
+            cur_tkn.append(ch)
+            esc_char = False
+            continue
+
+        if ch == '\\' and (not in_single or not in_double):
+            esc_char = not esc_char
+            if esc_char:
+                continue 
+        
+        if ch == "'" and not in_double:
             tkn_active = True
             in_single = not in_single
-        elif line[i] == '"' and not in_single:
+        elif ch == '"' and not in_single:
             tkn_active = True
             in_double = not in_double
-        elif line[i].isspace() and not in_single and not in_double:
+        elif ch.isspace() and not in_single and not in_double:
             if tkn_active:
                 tokens.append("".join(cur_tkn))
                 cur_tkn = []
@@ -56,14 +65,11 @@ def parse_line(line):
         else: 
             if not tkn_active:
                 tkn_active = True
-            cur_tkn.append(line[i])
+            cur_tkn.append(ch)
     if tkn_active:
         tokens.append("".join(cur_tkn))
     return tokens
    
-
-
-
 BUILTINS = {
     "exit": lambda code=0, *_: sys.exit(int(code)),
     "echo": lambda *args: print(" ".join(args)),
@@ -89,15 +95,22 @@ def main():
         command_name = user_Input[0]
 
         args = user_Input[1:]
-        
-        resolved = find_executable(command_name)
 
         if command_name in BUILTINS:
             BUILTINS[command_name](*args)       
-        elif resolved is not None:
-                subprocess.run([command_name] + args, executable=resolved)
-        else:    
-            print(f"{command_name}: command not found")
+        else:
+            resolved = find_executable(command_name)
+            if resolved is None:
+                print(f"{command_name}: command not found")
+            else:
+                try:
+                    subprocess.run([command_name] + args, executable=resolved)
+                except FileNotFoundError:
+                    print(f"{command_name}: command not found")
+                except PermissionError:
+                    print(f"{command_name}: permission denied")
+                except OSError as e:
+                    print(f"Error executing {command_name}: {e}")
 
 if __name__ == "__main__":
     main()
